07bec99cb2fb6d6eb5904a47e8d20f789016f88b
p-for-pratik <pratik9170713729@gmail.com>
2023-12-19 16:19:41 +0530

    fixed cors issue

--- src/main/java/com/flexmoney/MyYoga/Controllers/UserEnrollmentController.java
+++ src/main/java/com/flexmoney/MyYoga/Controllers/UserEnrollmentController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 // UserEnrollmentController.java
 @RestController
 @RequestMapping("/enroll")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*")
 public class UserEnrollmentController {
 
     @Autowired
