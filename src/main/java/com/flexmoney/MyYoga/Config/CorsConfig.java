fa2aaeed3549cf66693f2ee323c3a51dffb55e2a
p-for-pratik <pratik9170713729@gmail.com>
2023-12-19 17:45:22 +0530

    resolved forbidden issue

--- src/main/java/com/flexmoney/MyYoga/Config/CorsConfig.java
+++ src/main/java/com/flexmoney/MyYoga/Config/CorsConfig.java
@@ -3,20 +3,34 @@ package com.flexmoney.MyYoga.Config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
+@EnableWebMvc
+public class  CorsConfig implements WebMvcConfigurer {
 
-    @Bean
-    WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**") // You can specify a path pattern here
-                        .allowedOrigins("*") // Allow requests from this origin
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS"); // Specify allowed HTTP methods
-            }
-        };
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://yogaclass-client.vercel.app")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .exposedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
     }
+//    @Bean
+//    WebMvcConfigurer corsConfigurer() {
+//        return new WebMvcConfigurer() {
+//            @Override
+//            public void addCorsMappings(CorsRegistry registry) {
+//                registry.addMapping("/**") // You can specify a path pattern here
+//                        .allowedOrigins("*") // Allow requests from this origin
+//                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS"); // Specify allowed HTTP methods
+//            }
+//        };
+//    }
+//
+// }
 }
